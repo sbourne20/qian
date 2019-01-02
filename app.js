2f706bcf611e31e96b1f6a48b5b654af7f23fff6
Iwan Budihalim <iwan_budihalim@visuallabs.net>
2019-01-02 18:18:10 +0700

    ok

--- app.js
+++ app.js
@@ -5,12 +5,13 @@
         .module('qianApp', ['ngRoute', 'ngCookies','ui.router','ui.bootstrap','oc.lazyLoad'])
         .config (config)
         //.constant('DREAM_FACTORY_URL', 'https://sgproject001.bit-clicks.com:443')
-        .constant('DREAM_FACTORY_URL', 'http://192.168.8.48/api/v2/qiandb')
+        //.constant('DREAM_FACTORY_URL', 'http://192.168.8.48/api/v2/qiandb')
+        .constant('DREAM_FACTORY_URL', 'https://arjuna.bit-clicks.com/api/v2/qiandb')
         .run(run)
         .controller('mainController', mainController);
 
 
-
+        //MetronicApp.constant('DREAM_FACTORY_URL', 'https://arjuna.bit-clicks.com/api/v2/qiandb');
     config.$inject = ['$routeProvider' ];
     function config($routeProvider) {
 
